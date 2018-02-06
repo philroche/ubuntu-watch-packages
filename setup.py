c61c9449731760457540f12c8677fe80a82b6d28
Phil Roche <phil.roche@canonical.com>
2018-02-06 19:39:56 +0000

    Refactor to separate directory for easier setup.py and pip installation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from glob import glob
 from os.path import basename
 from os.path import splitext
 
-reqs_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+reqs_path = os.path.join(os.path.dirname(__file__), 'src/requirements.txt')
 
 with open(reqs_path, 'r') as req_file:
     dependencies = req_file.readlines()
@@ -19,9 +19,9 @@ setup(
     author_email='phil.roche@canonical.com',
     description='Helpful utility to watch specified packages, using rmadison, '
                 'in the ubuntu archive for transition between archive pockets',
-    packages=find_packages('.'),
-    package_dir={'': '.'},
-    py_modules=[splitext(basename(path))[0] for path in glob('*.py')],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     entry_points={
         'console_scripts': [
